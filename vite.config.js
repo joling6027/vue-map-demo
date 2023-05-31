78630968cb5b97b291de587a98113b2b9f276174
Joling Weng <joling6027@gmail.com>
2023-05-31 10:38:02 -0700

    Revise dist and add route base in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-map-private/',
+  base: '/vue-map-demo/',
   plugins: [vue()],
   resolve: {
     alias: {
