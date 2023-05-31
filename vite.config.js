e2f1db951ec23b693b3d0b10677373c153a54663
Joling Weng <joling6027@gmail.com>
2023-05-31 13:07:56 -0700

    modified vite.config to original settings

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-map-private/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    rollupOptions: {
-      external: ['vuetify/lib'], // Mark 'vuetify/lib' as external
-    },
-    define: {
-      'process.env.VITE_APP_GOOGLE_API_KEY': JSON.stringify(process.env.VITE_APP_GOOGLE_API_KEY),
-    },
   },
 })
